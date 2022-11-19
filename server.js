024aeabc1c4495dedd5f2db48c88598d94940242
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 15:04:15 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+let cors = require('cors')
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(cors({ origin: 'https://trello.com' }));
+
+
