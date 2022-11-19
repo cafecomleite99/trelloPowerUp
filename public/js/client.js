0e0075d61309f36d1cf9440f85b4985fb4dd027e
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 15:33:54 -0300

    Sla

--- public/js/client.js
+++ public/js/client.js
@@ -9,6 +9,12 @@ TrelloPowerUp.initialize({
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Estimate Size',
+      callback: (t, options) => {
+          return t.popup({
+            title: "Leonardo",
+            url: 'estimate.html'
+          })
+      }
     }];
   },
 });
