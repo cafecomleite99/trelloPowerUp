4ffc054e5613015133194ab6cc15a765866b503d
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 16:09:10 -0300

    Sla

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,16 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
 
+const bodyData = `{
+  "idModel": "5abbe4b7ddc1b351ef961414",
+  "modelType": "board",
+  "name": "<string>",
+  "type": "checkbox",
+  "options": "<string>",
+  "pos": "top",
+  "display_cardFront": true
+}`;
+
 function create() {
   fetch('https://api.trello.com/1/customFields?key=05b6872aa32005c40dec2e18df2576bb&token=09ec518306f076af868868f8183c8f26c7df9a2baf86d4a9e833f49ec4f1af65', {
     method: 'POST',
