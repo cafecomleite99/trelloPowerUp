324ae6b7c4a649b57dd7649480c42c57e89ac206
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 16:46:10 -0300

    Sla

--- public/js/client.js
+++ public/js/client.js
@@ -52,13 +52,13 @@ TrelloPowerUp.initialize({
   },
 });
 
-TrelloPowerUp.authorize({
+window.Trello.authorize({
   type: 'popup',
   name: 'Getting Started Application',
   scope: {
     read: 'true',
     write: 'true' },
   expiration: 'never',
-  success: () => console.log("ok"),
-  error: () => console.log("Falhou!")
+  success: authenticationSuccess,
+  error: authenticationFailure
 });
