6868e496c059a66029a5bc7a305920f020964ce8
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 15:49:11 -0300

    Sla

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,5 +1,15 @@
 let t = TrelloPowerUp.iframe()
 
+window.estimate.addEventListener('submit', function(event){
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+  .then(function(){
+    t.closePopup();
+  });
+});
+
+
 t.render(function(){
   t.sizeTo('#estimate').done();
 });
