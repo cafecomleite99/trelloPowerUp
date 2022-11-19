e31b882332bb5cf07f79b26e919d5335bf18dc97
Leonardo Sousa <leonardoiself2@gmail.com>
2022-11-19 16:13:31 -0300

    Sla

--- public/js/client.js
+++ public/js/client.js
@@ -7,9 +7,9 @@ var BLACK_ROCKET_ICON = "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9
 const bodyData = `{
   "idModel": "5abbe4b7ddc1b351ef961414",
   "modelType": "board",
-  "name": "<string>",
+  "name": "Tipo",
   "type": "checkbox",
-  "options": "<string>",
+  "options": "Consignado",
   "pos": "top",
   "display_cardFront": true
 }`;
